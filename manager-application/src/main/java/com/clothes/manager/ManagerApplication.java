079bd7d2bf26c670aad8b84fb3623a638c699d09
Ilia Chuvikin <iliachuvikinwork@mail.ru>
2025-02-15 19:37:11 +0300

    Проект из 4 модулей с неудавшимся docker-compose.yml

--- manager-application/src/main/java/com/clothes/manager/ManagerApplication.java
+++ manager-application/src/main/java/com/clothes/manager/ManagerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ManagerApplication {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(ManagerApplication.class);
-        app.setAdditionalProfiles("standalone");
+        app.setAdditionalProfiles("docker");
         app.run(args);
     }
 }

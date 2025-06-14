25592375da6693d78f5a2f9765350d9582a3288f
Ilia Chuvikin <iliachuvikinwork@mail.ru>
2025-06-14 15:15:18 +0300

    Написал dockerfile, docker-compose.yml, application-docker.yml

--- catalogue-service/src/main/java/com/clothes/catalogue/model/Product.java
+++ catalogue-service/src/main/java/com/clothes/catalogue/model/Product.java
@@ -46,7 +46,7 @@ public class Product {
     @Column(nullable = false)
     private String description;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id", nullable = false)
-    private Category category;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "category_id", nullable = false)
+//    private Category category;
 }
--- manager-application/src/main/java/com/clothes/manager/dto/Product.java
+++ manager-application/src/main/java/com/clothes/manager/dto/Product.java
@@ -3,6 +3,7 @@ package com.clothes.manager.dto;
 public record Product(
         Integer id,
         String title,
-        String description,
-        Integer categoryId) {
+        String description
+//        Integer categoryId
+) {
 }
